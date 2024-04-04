64da9213cfd1dc83c5813f76a192f52a892c924c
IgnacioAriza <141934199+IgnacioAriza@users.noreply.github.com>
2024-04-04 20:50:43 -0300

    Add files via upload

---
+++ Cuadrado.java
@@ -0,0 +1,28 @@
+package Ejercicio1;
+
+public class Cuadrado implements Figura, Rotable{
+    private float lado;
+
+    public Cuadrado(float lado) {
+        this.lado = lado;
+    }
+
+    public float getLado() {
+        return lado;
+    }
+
+    public void setLado(float lado) {
+        this.lado = lado;
+    }
+
+
+    @Override
+    public float area() {
+        return lado*lado;
+    }
+
+    @Override
+    public void rotar(){
+        System.out.println("Esta rotando el cuadrado: ■ ◆ ■ ◆ ■ ◆ ■ ◆ ■ ◆ ■");
+    }
+}
---
+++ Dibujable.java
@@ -0,0 +1,6 @@
+package Ejercicio1;
+
+public interface Dibujable {
+
+    public void dibujar();
+}
